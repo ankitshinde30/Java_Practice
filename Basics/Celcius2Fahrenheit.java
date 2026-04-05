05c20d0b9a0b413ab7bb2b9aeabc57fefeeb18b8
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-05 12:52:19 +0530

    Celcius to Fahrenheit conversion

---
+++ Basics/Celcius2Fahrenheit.java
@@ -0,0 +1,9 @@
+public class Celcius2Fahrenheit{
+    public static void main(String args[]){
+        double celcius= 34.6;
+
+        double fahrenheit=(celcius*9/5)+32;
+
+        System.out.println(celcius+ "ºC= " + fahrenheit+"ºF");
+    }
+}
