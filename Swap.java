60286d51d6abfe1d786d649ab4622d9734d05959
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-05 12:02:13 +0530

    Swap two Numbers using a 3rd variable

---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args) {
+        int num1=23;
+        int num2=45;
+        int temp;  //used the 3rd Variable to swap
+        temp =num1;
+        num1=num2;
+        num2=temp;
+        System.out.println("swaped num1 from 23 to "+ num1);
+        System.out.println("swaped num2 from 45 to "+ num2);
+    }
+}
