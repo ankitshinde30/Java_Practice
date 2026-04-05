df0638cf1b69d74a32e6ff5fdc11677d02973a3d
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-05 11:48:31 +0530

    added all class file and Operators Code

---
+++ Operators.java
@@ -0,0 +1,12 @@
+class Operators
+{
+	public static void main(String args[]){
+		
+	int num1, num2, result;
+	num1=5;
+	num2=3;
+	System.out.println((num1|num2));
+	System.out.println((num1^num2));
+}
+
+}
