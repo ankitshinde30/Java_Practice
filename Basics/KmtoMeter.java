c9a4f4e9b81e419a7a283e2663bbe661d7560b60
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-05 23:15:35 +0530

    Convert KM to Meter

---
+++ Basics/KmtoMeter.java
@@ -0,0 +1,9 @@
+public class KmtoMeter {
+    public static void main(String args[]){
+        double km = 4.6;
+
+        double meter = km * 1000;
+
+        System.out.println(km+" km = "+meter+" m");
+    }
+}
