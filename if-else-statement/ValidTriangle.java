ab6309bae459d773aebcc5bbb460ce20a218ec89
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-06 23:24:59 +0530

    Check if the Give Triangle is Valid or Not

---
+++ if-else-statement/ValidTriangle.java
@@ -0,0 +1,11 @@
+class ValidTriangle{
+public static void main(String args[]){
+int a=20,b=20,c=23;
+
+if(a+b>c&& a+c>b&&b+c>a ){
+System.out.println("Yes its an Valid Triangle");
+}else{
+System.out.println("no it is not an  Valid Triangle");
+}
+}
+}
