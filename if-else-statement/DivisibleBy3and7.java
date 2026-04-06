71316e1aae48c64f4a5fcaa1f7985cb86ffba311
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-06 23:14:18 +0530

    Check divisibility by 3 and 7 using if else

---
+++ if-else-statement/DivisibleBy3and7.java
@@ -0,0 +1,11 @@
+public class DivisibleBy3and7{
+public static void main(String args[]){
+int a= 21;
+
+if(a%3==0&& a%7==0){
+System.out.println("Yes its Divisible By 3&7");
+}else{
+System.out.println("no it is not Divisible by 3&7");
+}
+}
+}
