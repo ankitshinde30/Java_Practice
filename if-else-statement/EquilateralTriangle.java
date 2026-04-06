53c97eb7a156f3c8c87d108f3006332a15b31162
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-06 23:20:33 +0530

    Check of a Triangle is an Equilateral Traingle

---
+++ if-else-statement/EquilateralTriangle.java
@@ -0,0 +1,11 @@
+class EquilateralTriangle{
+public static void main(String args[]){
+int a=20,b=20,c=23;
+
+if(a==b && b==c && c==a ){
+System.out.println("Yes its an Equilateral Triangle");
+}else{
+System.out.println("no it is not an  Equilateral Triangle");
+}
+}
+}
