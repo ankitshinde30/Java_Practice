576682399bb354506a4367c46e8fed45ca10d970
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-06 23:16:01 +0530

    Check divisibility by 5 and 11 using if else

---
+++ if-else-statement/DivisibleBy5and11.java
@@ -0,0 +1,11 @@
+public class DivisibleBy5and11{
+public static void main(String args[]){
+int a= 55;
+
+if(a%5==0&& a%11==0){
+System.out.println("Yes its Divisible By 5");
+}else{
+System.out.println("no it is not Divisible by 5");
+}
+}
+}
