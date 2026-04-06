3f8099abb5b10c7c78e667054041c466203010b5
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-06 23:23:05 +0530

    Check if the Given Character is An Vowel or not

---
+++ if-else-statement/Vowel.java
@@ -0,0 +1,10 @@
+class Vowel{
+public static void main(String args[]){
+char c = 'a';
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+System.out.println("Yes The Given Character Lies In the Vowel");
+}else{
+System.out.println("no it is ot an Vowel");
+}
+}
+}
