5bf816ad658e7476991b6ea12afe1df32a2e4f91
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-06 23:24:00 +0530

    Check if the Given no is positive or not

---
+++ if-else-statement/Positive.java
@@ -0,0 +1,10 @@
+class Positive{
+public static void main(String args[]){
+int num= 24;
+if(num>=0){
+System.out.println("yes it is Positive number");
+}else{
+System.out.println("no it is Negative number");
+}
+}
+}
