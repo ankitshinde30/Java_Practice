d5224bf13ba17bdfa58c7e0115b357150a514c18
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-06 23:21:35 +0530

    Check if a no is even or odd

---
+++ if-else-statement/EvenOdd.java
@@ -0,0 +1,10 @@
+class EvenOdd{
+public static void main(String args[]){
+int num= 24;
+if(num%2==0){
+System.out.println("yes it is Even number");
+}else{
+System.out.println("no it is ODD number");
+}
+}
+}
