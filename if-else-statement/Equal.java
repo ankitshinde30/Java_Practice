78067f7d6c25f940016a725198f79d2fed010cea
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-06 23:18:02 +0530

    Check If Both number are EquAL or not

---
+++ if-else-statement/Equal.java
@@ -0,0 +1,11 @@
+class Equal{
+public static void main(String args[]){
+int num1= 24;
+int num2= 23;
+if(num1==num2){
+System.out.println("yes it is Equal number");
+}else{
+System.out.println("no it is not Equal number");
+}
+}
+}
