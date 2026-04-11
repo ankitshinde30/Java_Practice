a616506ade9b394c2199ce3f0c1fc81c0e949ff9
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-04-11 22:15:58 +0530

    Square Pattern number 3

---
+++ Loops/for-loop/patterns/SquarePattern3.java
@@ -0,0 +1,20 @@
+public class SquarePattern3 {
+    
+    public static void main(String[] args) {
+        
+        // Define number of rows and columns
+        int rows = 5;      // Number of rows (vertical)
+        int columns = 5;   // Number of columns (horizontal)
+        
+        // Outer loop → Controls ROWS
+        for (int row = 1; row <= rows; row++) {
+            
+            // Inner loop → Controls COLUMNS
+            for (int col = columns ; col >= 1; col--) {
+                System.out.print(col + " ");               }
+            
+            // After completing one row, move to next line
+            System.out.println();
+        }
+    }
+}
