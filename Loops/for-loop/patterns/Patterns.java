d296791f166dc33a121ef1cae673ee9058a7085e
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-05-03 21:59:56 +0530

    Hollow Rhombus pattern

--- Loops/for-loop/patterns/Patterns.java
+++ Loops/for-loop/patterns/Patterns.java
@@ -84,13 +84,29 @@ public class Patterns {
         }
     }
 
+    public static void hollow_rhombus(int n){
+         for(int i=1; i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n ||j==1 || j==n ){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+                System.out.println();
+    }
+}
 
     public static void main(String[] args) {
         // inverted_rotated_half_pyramid(5);
         // floyd_triangle(5);
         // zero_one_triangle_pattern(5);
         // Butterfly_pattern(4);
-        solid_rhombus(5);
+        // solid_rhombus(5);
+        hollow_rhombus(8);
         
     }
 }
