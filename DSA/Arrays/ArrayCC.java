b41aeb1909bd7ceb5f08a33ed13a6ac5418d6950
Ankitshinde30 <ankit.shinde2611@gmail.com>
2026-05-10 22:42:48 +0530

    Arrays As Functions Arguements

---
+++ DSA/Arrays/ArrayCC.java
@@ -0,0 +1,22 @@
+package Arrays;
+// import java.util.*;
+
+public class ArrayCC {
+    // Arrays As Functions Arguements
+    public static void update(int marks []){
+        for(int i=0;i<marks.length;i++){
+            marks[i]=marks[i]+2;
+
+        }
+
+    }
+    public static void main(String[] args) {
+        int marks []={95,96,97};
+
+        update( marks);
+
+        for(int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+" ");
+        }
+    }
+}
